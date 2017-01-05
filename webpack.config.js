231f66a3b084c2286a8eec4d219e39d0ebf8c114
Mark Katerberg <mark.katerberg@rgare.com>
2017-01-05 15:11:58 -0600

    Upgrades to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,36 @@
-var webpack = require('webpack');
-var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public'); 
-var APP_DIR = path.resolve(__dirname, 'src/client/app'); 
-
-var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        include: APP_DIR,
-        loader: 'babel'
-      }
-    ]
-  }
+const PATHS = {
+  build: path.resolve(__dirname, 'src/client/public'),
+  app: path.resolve(__dirname, 'src/client/app') 
 };
 
-module.exports = config;
+const common = merge(
+  {
+    entry: PATHS.app + '/index.jsx',
+    output: {
+      path: PATHS.build,
+      filename: 'bundle.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Initiative Tracker'
+      })
+    ],
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?/,
+          include: PATHS.app,
+          loader: 'babel-loader'
+        }
+      ]
+    }
+  }
+);
+
+module.exports = function(env) {
+  return merge(common);
+};
