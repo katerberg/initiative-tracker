a25bb0cd6d0a17b5031191047ddd36cc17dca4db
Mark Katerberg <mark.katerberg@rgare.com>
2017-01-05 12:50:13 -0600

    Uses babel and multiple jsx files

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,23 @@
-console.log('Hello World');
+import React from 'react';
+import {render} from 'react-dom';
+
+import AwesomeComponent from './AwesomeComponent.jsx';
+
+class App extends React.Component {
+
+  render() {
+    var components = [];
+    for (let i = 0; i < 10; i++) {
+      components.push(<AwesomeComponent key={i}/>);
+    }
+
+    return (
+      <div>
+        <p>Hello World!</p>
+        {components}
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
